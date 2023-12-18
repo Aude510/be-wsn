8b6844a083c118093d0bff11e292dd264a916e04
RomainMln <moulin.romain@hotmail.com>
2023-12-18 09:40:24 +0100

    Avec les fonctions c'est mieux

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import threading
 
 def init_threads():
-    
+    thread_reception = threading.Thread(target=thread_reception)
+    thread_envoi = threading.Thread(target=thread_envoi)
+    thread_reception.start()
+    thread_envoi.start()
+    return
+
+def thread_reception():
+    return
+
+def thred_envoie():
+    return
 
 
 
 if __name__ == "__main__":
+    init_threads()
