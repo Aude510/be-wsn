f83704da380a4381ce93a13f4b8e53da445c323f
Cyprien Heusse <c.heusse@gmail.com>
2023-12-20 10:15:55 +0100

    Node & Gateway functional

--- end-node/main.py
+++ end-node/main.py
@@ -1,55 +1,62 @@
+import sys
+
+sys.path.append('../')
+
 import threading
 import zmq_links
 import queue
-import constantsMAC
-import encoder
 import decoder
+import encoder
+import constantsMAC
 
 buffer_send = queue.Queue()
 mutex_buff_send = threading.Condition()
-seq = 0
 addr_node = 0x02
+
 def init_threads():
     threading.Thread(target=thread_send).start()
     threading.Thread(target=thread_add_data).start()
 
 def thread_send():
-    send_link = zmq_links.SendLink()
-    rcv_link = zmq_links.RecvLink()
+    seq = 0
+    send_link = zmq_links.SendLink("5557")
+    rcv_link = zmq_links.RecvLink("5558")
     while True:
         ##TODO timer et resend
+        data = None
         mutex_buff_send.acquire()
-        data = buffer_send.get()
+        if not buffer_send.empty():
+          data = buffer_send.get()
         mutex_buff_send.release()
-        send_link.send(data)
+        if data is None:
+          continue
+        sendPacket = encoder.Encoder(seq, constantsMAC.ADDR_GATEWAY, addr_node, data)
+        send_link.send(sendPacket.bytes())
         ack_received = False
-        while not(ack_received):    
+        while not ack_received:    
             packet = rcv_link.receive() ##TODO tester si packet est vide ou pas
             if(packet != None):
                 ack = decoder.Decoder(packet)
-                if(ack.dst_addr != addr_node):
+                if(ack.dst_addr() != addr_node):
                     continue
                 if(ack.is_ack() and ack.seq() == seq):
-                    seq = seq + 1
+                    seq += 1
                     ack_received = True
 
 
-
-
-
 def thread_add_data():
+    global seq
     while True:
-        msg = input("Data to send ?")
-        mutex_buff_send.acquire()
-        if(isFloat(msg)):
-            data = float(msg)
-        elif(isInt(msg)):
-            data = int(msg)
+        msg = input("Data to send ? ")
+        if(isInt(msg)):
+           data = int(msg)
+        elif(isFloat(msg)):
+           data = float(msg)
         else:
-            print("Error invalid type: please enter int or float")
-            continue
-        packet = encoder.Encoder(seq,constantsMAC.ADDR_GATEWAY,0x01,data)
-        buffer_send.put(packet.bytes())
+           print("Error invalid type: please enter int or float")
+           continue
+        mutex_buff_send.acquire()
+        buffer_send.put(data)
         mutex_buff_send.release()
     
 
@@ -68,5 +75,5 @@ def isInt(msg):
         return False
     
 if __name__ == "__main__":
-    addr_node = input("Quelle est l'adresse du device ?")
+    addr_node = int(input("Quelle est l'adresse du device ? "))
     init_threads()
