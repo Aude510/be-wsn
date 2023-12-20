797050fe1471e7a12d713d07c0bfa6e6fdaf5761
Cyprien Heusse <c.heusse@gmail.com>
2023-12-20 11:15:20 +0100

    Revert "Reset to before Romain"
    This reverts commit 279533ba1e770b1fa8270951a04c3df02b343d60.

--- gateway/main.py
+++ gateway/main.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append("../")
+sys.path.append('../')
 
 import threading
 import zmq_links
@@ -8,7 +8,6 @@ import queue
 import decoder
 import encoder
 import constantsMAC
-import requests
 
 buffer_receive = queue.Queue()
 mutex_buff_rcv = threading.Condition()
@@ -28,8 +27,8 @@ def thread_reception():
             mutex_buff_rcv.release()
 
 def thread_decode():
+    send_link = zmq_links.SendLink("5558")
     while True:
-        send_link = zmq_links.SendLink("5558")
         mutex_buff_rcv.acquire()
         if(not(buffer_receive.empty())):
             packet = decoder.Decoder(buffer_receive.get())
@@ -42,23 +41,13 @@ def thread_decode():
                 sequence[packet.src_addr()]+=1
                 print("La valeur dans le paquet est : " + str(packet.value()))
                 ## TODO : faire un call API
-                if(packet.code()==constantsMAC.CODE_DATA_INT):
-                    requests.post('https://api.is-grandma-alive.obrulez.fr/mamie-debout',json={'debout':str(packet.value())})
-                elif(packet.code() == constantsMAC.CODE_DATA_FLOAT):
-                    requests.post('https://api.is-grandma-alive.obrulez.fr/mamie-asphyxie',json={'asphyxie':str(packet.value())})
-                else:
-                    print("Code not reconized")
-            elif(packet.dst_addr== constantsMAC.ADDR_GATEWAY and sequence[packet.src_addr()]<packet.seq()):
+            elif(packet.dst_addr == constantsMAC.ADDR_GATEWAY and sequence[packet.src_addr()]<packet.seq()):
                 sequence[packet.src_addr()] = packet.seq()
                 print("Numéro de séquence de futur")
                 print("La valeur dans le paquet est : " + str(packet.value()))
-                if(packet.code()==constantsMAC.CODE_DATA_INT):
-                    requests.post('https://api.is-grandma-alive.obrulez.fr/mamie-debout',json={'debout':str(packet.value())})
-                elif(packet.code() == constantsMAC.CODE_DATA_FLOAT):
-                    requests.post('https://api.is-grandma-alive.obrulez.fr/mamie-asphyxie',json={'asphyxie':str(packet.value())})
-                else:
-                    print("Code not reconized")
-            ack = encoder.Encoder(packet.seq(),packet.src_addr(),packet.dst_addr(),None,True)
+                ## TODO faire aussi un call API
+            ack = encoder.Encoder(packet.seq(), packet.src_addr(), packet.dst_addr(), None, True).bytes()
+            dec_ack = decoder.Decoder(ack)
             send_link.send(ack)                
         mutex_buff_rcv.release()
 
